c4950047fa26b28837d96a3a9c5d5601b4c0c0cf
Javier Méndez <eltxabi@gmail.com>
2021-01-08 22:03:14 +0100

    Base de datos libros

--- src/controlador/BibliotecaController.java
+++ src/controlador/BibliotecaController.java
@@ -1,6 +1,7 @@
 package controlador;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,10 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import modelo.Libro;
+import modelo.LibroDAO;
+
 /**
  * Servlet implementation class BibliotecaController
  */
-@WebServlet("")
+@WebServlet(urlPatterns ={"","/insertar"})
 public class BibliotecaController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,11 +31,36 @@ public class BibliotecaController extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("Servlet BibliotecaController");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		RequestDispatcher despachador = null;
-		despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+		if (request.getServletPath().equals("")) {
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				ArrayList<Libro> libros;
+				libros = libroDAO.getLibros();
+				request.setAttribute("libros", libros);
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("error",e.getMessage());
+			}
+			despachador = request.getServletContext().getRequestDispatcher("/index.jsp");
+		} else if (request.getServletPath().equals("/insertar")) {
+			try {
+				LibroDAO libroDAO = new LibroDAO();
+				Libro libro = new Libro(Integer.parseInt(request.getParameter("isbn")),
+						request.getParameter("titulo"),request.getParameter("autor"));
+				libroDAO.insertar(libro);
+				request.setAttribute("info", "Libro "+ libro +" añadido");
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("error",e.getMessage());
+			} catch (RuntimeException e) {
+				// TODO Auto-generated catch block
+				request.setAttribute("error",e.getMessage());
+			}
+			despachador = request.getServletContext().getRequestDispatcher("/");
+		}
 		despachador.forward(request, response);
 	}
 
